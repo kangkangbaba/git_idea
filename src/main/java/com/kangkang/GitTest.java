11562545f6942a0e46bceba93337a1c823eb9735
kangkang <1145413114@qq.com>
2023-01-30 11:32:25 +0800

    first commit

---
+++ src/main/java/com/kangkang/GitTest.java
@@ -0,0 +1,13 @@
+package com.kangkang;
+
+/**
+ * @PROJECT_NAME: git_idea
+ * @DESCRIPTION:git的测试
+ * @USER: kangl
+ * @DATE: 2023/1/30 11:21
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
